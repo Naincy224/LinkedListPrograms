0ec31503143b9ae8328260e7e80bcb5105e0577e
Naincy224 <pihusingh224@gmail.com>
2021-11-19 21:07:36 +0530

    Modified: simple linked list program to added linked list program

--- src/com/bridgeit/MyNodeTest.java
+++ src/com/bridgeit/MyNodeTest.java
@@ -4,21 +4,15 @@ class MyNodeTest {
     private static Node head=null;
 	public static void main(String[] args) {
 
-		Node n1= new Node(56);
-		head=n1;
-		Node n2= new Node(30);
-		Node n3=new Node(70);
-		n1.setNext(n2);
-		n2.setNext(n3);
 		
-		System.out.println("Nodes of Linklist:");
-		 	Node start = head;  
-	        while (start != null)  
-	        {  	
-	        	start.printNodeData();  
-	        	start = start.getNext();  
-	        }  
+		//created Linked List by adding 30 and 56 to 70
+
+	    LinkedList list = new LinkedList();
+		list.addElement(56);
+		list.addElement(30);
+		list.addElement(70);
 		
+		list.display();
 	}
 
 }
